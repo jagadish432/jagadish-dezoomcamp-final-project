e965e670bab3afc4b4e8e8bf822831dd1fbb5c7f
Jagadeesh Dachepalli <jagadish.dachepalli@gmail.com>
2023-04-09 07:54:05 +0000

    updated readme for dashboards with all info, and also a local python pyspark file'

--- README.md
+++ README.md
@@ -84,63 +84,34 @@ This is Final project by Jagadeesh Dachepalli as part of DataTalksClub DE Zoomca
 
 
 ## Spark pipeline
-### local running
-### spark-gcloud connection
+### local running with spark-gcloud connection for bigquery and gcp buckets
 0. make sure you logged in already using `gcloud auth login`
 1. copy the gcloud connector hadoop library from gcloud onto the local directory `gsutil cp gs://hadoop-lib/gcs/gcs-connector-hadoop3-2.2.5.jar lib/gcs-connector-hadoop3-2.2.5.jar` in order for us to be able to load data from files stored in the GCS bucket
 2. copy the gcloud connector bigquey library from gcloud to the local directory `gsutil cp gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar lib/spark-bigquery-latest_2.12.jar` in order for us to be able to read/write data from/to BigQuery.
+3. convert the jupyter notebook(which we could use for local testing) to a python file using `jupyter nbconvert --to=script generate_stats.ipynb `
+4. Edit the python file as per your needs.
+5. `cd pyspark_jobs/`
+5. now, run the python file `python generate_stats.py`
 
-#### TODO
-1. write a local pyspark job to read gcs parquet files
-2. comeup with the join conditions needed for matches and balls data to bring batting/bowling/team statistics
-3. save them to the bigquery table
-4. create a dataproc cluster using terraform
-5. execute the 1,2,3 points using dataproc spark job
+
 
 
 ## Dashboard
-1. create a google studio dashboard and fetch data from bigquery facts table created in the `Spark pipeline` step.
+Please remember that this IPL tournament has happened every year from 2008 to 2022 (15 editions so far..).
 
-+--------------------+
-|               team1|
-+--------------------+
-| Sunrisers Hyderabad|
-|Lucknow Super Giants|
-| Chennai Super Kings|
-|      Gujarat Titans|
-|Rising Pune Super...|
-|     Deccan Chargers|
-|Kochi Tuskers Kerala|
-|    Rajasthan Royals|
-|       Gujarat Lions|
-|Royal Challengers...|
-|Kolkata Knight Ri...|
-|Rising Pune Super...|
-|     Kings XI Punjab|
-|        Punjab Kings|
-|       Pune Warriors|
-|    Delhi Daredevils|
-|      Delhi Capitals|
-|      Mumbai Indians|
-+--------------------+
+1. *Dashboard - 1* : This is created using the same data we got from the datalake (ofcourse with a little changes over the raw data, these changes are done in the prefect pipeline before storing the data in Datalake)
+    a. This has 2 charts -  1st chart(table) shows number of matches played at each venue and the 2nd 
+    chart(pie graph) shows percentages of matches won by batting 1st vs batting 2nd vs matches won in an superover result.
+    b. URL - https://lookerstudio.google.com/reporting/5109b6af-c929-4dd2-9f27-be979d92761d/page/io1LD
 
+2. *Dashboard - 2* : This is created using the `team_stats` table which we created and inserted data in the pyspark pipeline using some SQL transformations (union, group by, sort). 
+    a. It has 2 pages with 2 different Graphs, please have a look at both of them.
+        i. the 1st page has 1 bar graph shows 'total number of matches won' by each team so far in this tournament
+        ii. the 2nd page has 1 bar graph again but it shows 'total number of matches palyed vs total number of matches won' by each team so far in this tournament.
+    b. URL - https://lookerstudio.google.com/reporting/590f3b48-bdb9-4bf3-924f-e75648bbfe74/page/Sq1LD
 
-1. merge old & new team names
-2. change team names to shortcuts
-3. change wonby values to batting 1 /2 instead of wickets and runs
-4. change 2007/8 to 2008, and 2009/10 to 2010, and 2020/21 to 2020
-
-
-which_batting = None
-if winning_team == toss_winning_team:
-    if toss_decision == 'field':
-        which_batting = 'batting2'
-    elif toss_decision == 'bat':
-        which_batting = 'batting1
-
-
-elif winning_team != toss_winning_team:
-    if toss_decision == 'field':
-        which_batting = 'batting1'
-    elif toss_decision == ' bat':
-        which_batting = 'batting2'
+3. *Dashboard - 3* : This is created using the `batting_stats` table/data which we created as part of the same pyspark pipeline using SQL transformations(Join, GroupBy, Sum aggregations, sort). 
+    a. This dashboard shows 'total number of runs scored' by each batsman for last 10 years (year-wise)
+        i. Also, it has some additional feature i.e., a drop-down to select 1 or multiple batsman
+        ii. The drop-down shows top 5 run-getters(batsmen).
+    b. URL - https://lookerstudio.google.com/reporting/1f70d4c6-ab99-4402-9af5-a2a3bf52c479/page/kl2LD
---
+++ pyspark_jobs/generate_stats.py
@@ -0,0 +1,111 @@
+import pyspark
+from time import time
+from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
+from pyspark.context import SparkContext
+
+start = time()
+# All this setup required to access GCS bucket and Bigquery, is only for local/VM machines only, 
+# not for the GCP dataproc clusters
+credentials_location = '/home/jagadish/.gc/finaldatazoomcamp.json'
+
+conf = SparkConf().setMaster("local[*]").setAppName('generate-stats-local') \
+        .set("spark.jars", "./lib/gcs-connector-hadoop3-2.2.5.jar,./lib/spark-bigquery-latest_2.12.jar") \
+        .set("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
+        .set("spark.hadoop.google.cloud.auth.service.account.json.keyfile", credentials_location)
+
+sc = SparkContext(conf=conf)
+
+hadoop_conf = sc._jsc.hadoopConfiguration()
+
+hadoop_conf.set("fs.AbstractFileSystem.gs.impl",  "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
+hadoop_conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+hadoop_conf.set("fs.gs.auth.service.account.json.keyfile", credentials_location)
+hadoop_conf.set("fs.gs.auth.service.account.enable", "true")
+
+spark = SparkSession.builder.config(conf=sc.getConf()).getOrCreate()
+
+# IPL matches data
+ipl_matches_data_gcs_path = "gs://jagadish_data_lake_datazoomcamp-jagadish-final/IPL_Matches_2008_2022.parquet"
+ipl_matches = spark.read.parquet(ipl_matches_data_gcs_path)
+
+# store matches data into BigQuery table
+ipl_matches.write.partitionBy("season").format("bigquery").mode('Overwrite') \
+    .option("parentProject", "datazoomcamp-jagadish-final") \
+    .option("temporaryGcsBucket","jagadish_data_lake_datazoomcamp-jagadish-final") \
+    .option('table', 'ipl_data.matches').save()
+# register as table to use as a SQL table using spark
+ipl_matches.registerTempTable('matches')
+
+# TEAM Stats
+# fetching all matches stats for teams mentioned in team1 column
+first_innings_stats = spark.sql("""
+select team1 as team, season, count(1) as matches_played, 
+sum(CASE
+    WHEN winning_team == team1 THEN 1
+    ELSE 0
+END ) AS matches_won
+from matches m
+group by team1, season
+""")
+
+# fetching all matches stats for teams mentioned in team2 column 
+second_innings_stats = spark.sql("""
+select team2 as team, season, count(1) as matches_played,
+sum(CASE
+    WHEN winning_team == team2 THEN 1
+    ELSE 0
+END ) AS matches_won
+from matches m
+group by team2, season
+""")
+
+teams_stats = first_innings_stats.unionAll(second_innings_stats)
+teams_stats = teams_stats.groupBy('team', 'season').sum('matches_played', 'matches_won') \
+    .sort('season', ascending=True)
+teams_stats = teams_stats.withColumnRenamed('sum(matches_played)', 'matches_played') \
+    .withColumnRenamed('sum(matches_won)', 'matches_won')
+
+# writing teams statistics yearwise
+teams_stats.write.format("bigquery").mode('Overwrite') \
+    .option("parentProject", "datazoomcamp-jagadish-final") \
+    .option("temporaryGcsBucket","jagadish_data_lake_datazoomcamp-jagadish-final") \
+    .option('table', 'ipl_data.teams_stats').save()
+
+# IPL ball by ball data for every match
+ipl_ball_by_ball_data_gcs_path = "gs://jagadish_data_lake_datazoomcamp-jagadish-final/IPL_Ball_by_Ball_2008_2022.parquet"
+ipl_ball_by_ball = spark.read.parquet(ipl_ball_by_ball_data_gcs_path)
+
+# store scores data into BigQuery table
+ipl_ball_by_ball.write.format("bigquery").mode('Overwrite') \
+    .option("parentProject", "datazoomcamp-jagadish-final") \
+    .option("temporaryGcsBucket","jagadish_data_lake_datazoomcamp-jagadish-final") \
+    .option('table', 'ipl_data.scores').save()
+# register as table to use as a SQL table using spark
+ipl_ball_by_ball.registerTempTable('ball_by_ball')
+
+# JOIN matches data and ball_by_ball data to get info/stats for each ball per each match happened
+result = spark.sql("""
+select b.id, b.batter, b.batsman_run, b.bowler, b.is_wicket_delivery, dismissal_type, m.season
+from ball_by_ball b
+join matches m
+on b.id = m.id
+""")
+
+# BATTING Stats
+batting_stats = result.groupBy('season', 'b.batter').sum('batsman_run') \
+    .withColumnRenamed("sum(batsman_run)", "runs").sort('season', "runs", ascending=False)
+
+# writing batting statistics yearwise
+batting_stats.write.partitionBy("season").format("bigquery").mode('Overwrite') \
+    .option("parentProject", "datazoomcamp-jagadish-final") \
+    .option("temporaryGcsBucket","jagadish_data_lake_datazoomcamp-jagadish-final") \
+    .option('table', 'ipl_data.batting_stats').save()
+
+# BOWLING Stats - but not stored anywhere as of now.
+bowling_stats = result.filter(result["dismissal_type"] != "run out").groupBy('season', 'b.bowler') \
+    .sum('is_wicket_delivery').withColumnRenamed("sum(is_wicket_delivery)", "wickets") \
+    .sort('season', "wickets", ascending=False)
+
+end = time()
+print("Total time taken by this job: ", end-start)
